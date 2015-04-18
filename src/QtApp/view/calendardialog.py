9623a2a124d055bfad12682a7745b2ee69b8b7f3
Rodney Rodriguez <rodneyxr@gmail.com>
2015-04-18 06:51:57 -0500

    fixed dumb errors

--- src/QtApp/view/calendardialog.py
+++ src/QtApp/view/calendardialog.py
@@ -1,52 +1,13 @@
-# -*- coding: utf-8 -*-
+import sys
+from PyQt4 import QtGui
+from QtApp.view.calendar_dialog_ui import Ui_calDialog
 
-# Form implementation generated from reading ui file 'calendardialog.ui'
-#
-# Created: Sat Apr 18 05:14:08 2015
-#      by: PyQt4 UI code generator 4.11.1
-#
-# WARNING! All changes made in this file will be lost!
+class CalendarDialog(QtGui.QDialog):
+    def __init__(self):
+        super(CalendarDialog, self).__init__()
+        self.initUI()
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
-
-class Ui_calDialog(object):
-    def setupUi(self, calDialog):
-        calDialog.setObjectName(_fromUtf8("calDialog"))
-        calDialog.resize(400, 300)
-        self.verticalLayoutWidget = QtGui.QWidget(calDialog)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(50, 40, 266, 174))
-        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
-        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setMargin(0)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.calendarWidget = QtGui.QCalendarWidget(self.verticalLayoutWidget)
-        self.calendarWidget.setObjectName(_fromUtf8("calendarWidget"))
-        self.verticalLayout.addWidget(self.calendarWidget)
-        self.checkBox = QtGui.QDialogButtonBox(self.verticalLayoutWidget)
-        self.checkBox.setOrientation(QtCore.Qt.Horizontal)
-        self.checkBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.checkBox.setObjectName(_fromUtf8("checkBox"))
-        self.verticalLayout.addWidget(self.checkBox)
-
-        self.retranslateUi(calDialog)
-        QtCore.QObject.connect(self.checkBox, QtCore.SIGNAL(_fromUtf8("accepted()")), calDialog.accept)
-        QtCore.QObject.connect(self.checkBox, QtCore.SIGNAL(_fromUtf8("rejected()")), calDialog.reject)
-        QtCore.QMetaObject.connectSlotsByName(calDialog)
-
-    def retranslateUi(self, calDialog):
-        calDialog.setWindowTitle(_translate("calDialog", "Dialog", None))
-
+    def initUI(self):
+        self.diag = Ui_calDialog()
+        self.diag.setupUi(self)
+        self.diag.checkBox.accepted.connect(self.done)
