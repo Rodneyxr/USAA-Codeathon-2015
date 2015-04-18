9aeb63e4751e475a1ecfb279d8f39a26f4430bcd
Rodney Rodriguez <rodneyxr@gmail.com>
2015-04-17 21:53:40 -0500

    fixed up test.py ready for tests initialized model and controller

---
+++ src/test.py
@@ -0,0 +1,30 @@
+import unittest
+import sys
+
+from PyQt4 import QtCore, QtGui
+from hashlib import sha256
+
+
+class Test(unittest.TestCase):
+    """
+     This class will test all kinds of functionality for this application.
+     All test method names should follow the naming convention of all lower-case
+     letters delimited by underscores and must begin with 'test_'
+     ex: def test_login(self):
+     """
+
+    def setUp(self):
+        self.app = QtGui.QApplication(sys.argv)
+        mainWindow = QtGui.QMainWindow()
+        mainWindow.show()
+
+    def tearDown(self):
+        sys.exit(self.app.exec())
+
+    def test_hash(self):
+        password = "password01"
+        print(sha256(str(password).encode('utf-8')).hexdigest())
+
+
+if __name__ == '__main__':
+    unittest.main()
