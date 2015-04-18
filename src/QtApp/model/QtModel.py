24a4638baab060efd8b400a1c8f00c0aefa5d166
Rodney Rodriguez <rodneyxr@gmail.com>
2015-04-18 04:02:49 -0500

    removed commented code

--- src/QtApp/model/QtModel.py
+++ src/QtApp/model/QtModel.py
@@ -11,31 +11,9 @@ class QtModel(QtCore.QObject):
         QtCore.QObject.__init__(self)
 
         self.header = ['FLIGHT ID', 'ORIGIN', 'DEST', 'DEPART', 'ARRIVE', 'AVAIL']
-        # self.data = [['ID01', 'San Antonio', 'Dallas', QtCore.QDateTime(2015, 1, 26, 11, 0),
-        #               QtCore.QDateTime(2015, 1, 26, 11, 0), 42],
-        #              ['ID02', 'San Fran', 'Las Vegas', QtCore.QDateTime(2015, 2, 27, 9, 0),
-        #               QtCore.QDateTime(2015, 2, 27, 3, 0), 99],
-        #              ['ID03', 'Hawaii', 'New York', QtCore.QDateTime(2015, 11, 1, 10, 0),
-        #               QtCore.QDateTime(2015, 11, 1, 5, 0), 128],
-        #              ['ID04', 'Tokyo', 'Houston', QtCore.QDateTime(2015, 12, 16, 14, 0),
-        #               QtCore.QDateTime(2015, 12, 16, 20, 0), 42]]
 
         data = FlightsDB.getFlights()
         self.flights = data
-
-        # for flight in data():
-        #
-        #
-        # for i in range(len(data)):
-        #     tmpFlight = self.createFlight(
-        #         data[0],
-        #         data[1],
-        #         data[i][2],
-        #         data[i][3],
-        #         data[i][4],
-        #         data[i][5])
-        #     self.addFlight(tmpFlight)
-
         self.__departureList = FlightStatusTable(self.flights, self.header)
 
     def getDepartureListModel(self):
