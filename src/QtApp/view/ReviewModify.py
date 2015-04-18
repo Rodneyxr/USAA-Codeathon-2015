2fea27730fe624a7057c7abed8bb3c3269fd0e40
PySean <smsod18@hotmail.com>
2015-04-18 07:46:34 -0500

    review modify, woops

---
+++ src/QtApp/view/ReviewModify.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt4 import QtGui, QtCore
+from QtApp.view.review_modify import Ui_ReviewModifyForm
+
+class ReviewModify(QtGui.QWidget):
+
+    signalClear = QtCore.pyqtSignal(int)
+    def __init__(self, model):
+        super(ReviewModify, self).__init__()
+        self.initUI(model)
+
+    def initUI(self, model):
+        self.review = Ui_ReviewModifyForm()
+        self.review.setupUi(self)
+        self.review.cancelSelected.clicked.connect(self.clearSelected)
+        self.review.table.setModel(model.getBookedListModel())
+    
+    def clearSelected(self, item):
+        self.signalClear.emit(item.row())
