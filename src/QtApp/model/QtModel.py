713afef87c7b139ed65d2d08a490bd3e9c89f87d
Rodney Rodriguez <rodneyxr@gmail.com>
2015-04-18 02:31:35 -0500

    added getting flights from DB

--- src/QtApp/model/QtModel.py
+++ src/QtApp/model/QtModel.py
@@ -3,6 +3,7 @@ from PyQt4 import QtCore
 from QtApp.model.FlightModel import FlightStatusTable
 from QtApp.model.FlightClass import Flight
 from QtApp.database.FlightsTable import FlightQueryNames
+from QtApp.database.FlightsDB import FlightsDB
 
 
 class QtModel(QtCore.QObject):
@@ -10,18 +11,19 @@ class QtModel(QtCore.QObject):
         QtCore.QObject.__init__(self)
 
         self.header = ['FLIGHT ID', 'ORIGIN', 'DEST', 'DEPART', 'ARRIVE', 'AVAIL']
-        self.data = [['ID01', 'San Antonio', 'Dallas', QtCore.QDateTime(2015, 1, 26, 11, 0),
-                      QtCore.QDateTime(2015, 1, 26, 11, 0), 42],
-                     ['ID02', 'San Fran', 'Las Vegas', QtCore.QDateTime(2015, 2, 27, 9, 0),
-                      QtCore.QDateTime(2015, 2, 27, 3, 0), 99],
-                     ['ID03', 'Hawaii', 'New York', QtCore.QDateTime(2015, 11, 1, 10, 0),
-                      QtCore.QDateTime(2015, 11, 1, 5, 0), 128],
-                     ['ID04', 'Tokyo', 'Houston', QtCore.QDateTime(2015, 12, 16, 14, 0),
-                      QtCore.QDateTime(2015, 12, 16, 20, 0), 42]]
+        # self.data = [['ID01', 'San Antonio', 'Dallas', QtCore.QDateTime(2015, 1, 26, 11, 0),
+        #               QtCore.QDateTime(2015, 1, 26, 11, 0), 42],
+        #              ['ID02', 'San Fran', 'Las Vegas', QtCore.QDateTime(2015, 2, 27, 9, 0),
+        #               QtCore.QDateTime(2015, 2, 27, 3, 0), 99],
+        #              ['ID03', 'Hawaii', 'New York', QtCore.QDateTime(2015, 11, 1, 10, 0),
+        #               QtCore.QDateTime(2015, 11, 1, 5, 0), 128],
+        #              ['ID04', 'Tokyo', 'Houston', QtCore.QDateTime(2015, 12, 16, 14, 0),
+        #               QtCore.QDateTime(2015, 12, 16, 20, 0), 42]]
 
+        data = FlightsDB.getFlights()
         self.flights = list()
 
-        for i in range(len(self.data)):
+        for i in range(len(data)):
             tmpFlight = self.createFlight(
                 self.data[i][0],
                 self.data[i][1],
